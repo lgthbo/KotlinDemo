be44668e6ff9f86737ca00610db95476799dfb34
lgthbo <lgthbo@gmail.com>
2020-05-31 17:44:32 +0800

    OnTouchEvent

--- app/src/main/java/com/example/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/example/kotlindemo/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.kotlindemo
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.MotionEventCompat
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -12,7 +16,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var heartImage = findViewById<View>(R.id.imageView)
+
+        heartImage.setOnTouchListener { _, event ->
+            Toast.makeText(applicationContext, event.toString(), Toast.LENGTH_SHORT).show()
+            // Log.i("Event String", event.toString())
+            if(event.action == MotionEvent.ACTION_DOWN)
+                Log.i("Event String", event.toString())
+            true
+        }
     }
+
+
+
 
     fun changePageToFortune(view: View){
         var myIntent = Intent(this,FortuneTelling::class.java)
--- app/src/main/java/com/example/kotlindemo/PictureManager.kt
+++ app/src/main/java/com/example/kotlindemo/PictureManager.kt
@@ -3,13 +3,24 @@ package com.example.kotlindemo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 
-class PictureManager : AppCompatActivity() {
+class PictureManager : AppCompatActivity(), View.OnClickListener { //add View.OnClickListener implementation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_picture_manager)
+        val microphone: ImageView = findViewById(R.id.imageView3)
+        microphone.setOnClickListener(this)
+
     }
+
+    override fun onClick(v: View?) {
+        Toast.makeText(applicationContext, R.string.onClick,LENGTH_SHORT ).show()
+    }
+
     fun goBack(view: View){
         this.finish()
     }
